9b85348df67c3ed42c892fe17796c284f4c5759e
Carlos W. Mercado <29048235+carloswm85@users.noreply.github.com>
2024-06-07 08:41:18 -0300

    add # Debug.WriteLine & timeout increment

--- Controllers/DatatablesController.cs
+++ Controllers/DatatablesController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Dynamic.Core;
 using System.Reflection;
@@ -14,218 +15,277 @@ using System.Threading.Tasks;
 
 namespace AspNetCoreDatatable.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DatatablesController : ControllerBase
-    {
-        private readonly AspNetCoreDatatableContext _context;
-        public DatatablesController(AspNetCoreDatatableContext context)
-        {
-            _context = context;
-        }
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DatatablesController : ControllerBase
+	{
+		private readonly AspNetCoreDatatableContext _context;
+		public DatatablesController(AspNetCoreDatatableContext context)
+		{
+			_context = context;
+		}
 
-        // Basic Datatable
-        [HttpGet]
-        public async Task<IActionResult> GetAsync()
-        {
-            return Ok(new { data = await _context.UserInfos.ToListAsync() });
-        }
+		// Basic Datatable
+		[HttpGet]
+		public async Task<IActionResult> GetAsync()
+		{
+			Debug.WriteLine("> BASIC");
+			return Ok(new { data = await _context.UserInfos.ToListAsync() });
+		}
 
-        // Pagination
-        [HttpPost("pagination")]
-        public async Task<IActionResult> PostPaginationAsync()
-        {
-            try
-            {
-                string draw = Request.Form["draw"].FirstOrDefault();
-                string start = Request.Form["start"].FirstOrDefault();
-                string length = Request.Form["length"].FirstOrDefault();
-                string searchValue = Request.Form["search[value]"].FirstOrDefault();
-                int pageSize = length != null ? Convert.ToInt32(length) : 0;
-                int skip = start != null ? Convert.ToInt32(start) : 0;
-                int recordsTotal = 0;
+		// Pagination
+		[HttpPost("pagination")]
+		public async Task<IActionResult> PostPaginationAsync()
+		{
+			Debug.WriteLine("> PAGINATION");
+			try
+			{
+				var watch = Stopwatch.StartNew();
+				watch.Start();
 
-                IQueryable<UserInfo> userInfo = (from dbuserinfo in _context.UserInfos select dbuserinfo);
-                if (!string.IsNullOrEmpty(searchValue))
-                {
-                    userInfo = userInfo.Where(m => m.Name.Contains(searchValue)
-                                                || m.Gender.Contains(searchValue)
-                                                || m.EyeColor.Contains(searchValue)
-                                                || m.Email.Contains(searchValue)
-                                                || m.Phone.Contains(searchValue)
-                                                || m.Company.Contains(searchValue));
-                }
+				string draw = Request.Form["draw"].FirstOrDefault();
+				string start = Request.Form["start"].FirstOrDefault();
+				string length = Request.Form["length"].FirstOrDefault();
+				string searchValue = Request.Form["search[value]"].FirstOrDefault();
+				int pageSize = length != null ? Convert.ToInt32(length) : 0;
+				int skip = start != null ? Convert.ToInt32(start) : 0;
+				int recordsTotal = 0;
 
-                recordsTotal = userInfo.Count();
-                List<UserInfo> data = pageSize < 0 ? await userInfo.ToListAsync() : await userInfo.Skip(skip).Take(pageSize).ToListAsync();
-                return Ok(new { draw, recordsFiltered = recordsTotal, recordsTotal, data });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                return BadRequest();
-                throw;
-            }
-        }
+				IQueryable<UserInfo> userInfo = (from dbuserinfo in _context.UserInfos select dbuserinfo);
+				if (!string.IsNullOrEmpty(searchValue))
+				{
+					userInfo = userInfo.Where(m => m.Name.Contains(searchValue)
+												|| m.Gender.Contains(searchValue)
+												|| m.EyeColor.Contains(searchValue)
+												|| m.Email.Contains(searchValue)
+												|| m.Phone.Contains(searchValue)
+												|| m.Company.Contains(searchValue));
+				}
 
-        // Searching
-        [HttpPost("searching")]
-        public async Task<IActionResult> PostSearchingAsync()
-        {
-            try
-            {
-                string draw = Request.Form["draw"].FirstOrDefault();
-                string start = Request.Form["start"].FirstOrDefault();
-                string length = Request.Form["length"].FirstOrDefault();
-                string searchValue = Request.Form["search[value]"].FirstOrDefault();
-                int pageSize = length != null ? Convert.ToInt32(length) : 0;
-                int skip = start != null ? Convert.ToInt32(start) : 0;
-                int recordsTotal = 0;
+				recordsTotal = userInfo.Count();
+				Debug.WriteLine(recordsTotal);
 
-                IQueryable<UserInfo> userInfo = (from dbuserinfo in _context.UserInfos select dbuserinfo);
-                if (!string.IsNullOrEmpty(searchValue))
-                {
-                    userInfo = userInfo.Where(m => m.Name.Contains(searchValue)
-                                                || m.Gender.Contains(searchValue)
-                                                || m.EyeColor.Contains(searchValue)
-                                                || m.Email.Contains(searchValue)
-                                                || m.Phone.Contains(searchValue)
-                                                || m.Company.Contains(searchValue));
-                }
+				List<UserInfo> data = new List<UserInfo>();
 
-                recordsTotal = userInfo.Count();
-                List<UserInfo> data = pageSize < 0 ? await userInfo.ToListAsync() : await userInfo.Skip(skip).Take(pageSize).ToListAsync();
-                return Ok(new { draw, recordsFiltered = recordsTotal, recordsTotal, data });
-            }
-            catch (Exception)
-            {
-                return BadRequest();
-                throw;
-            }
-        }
+				if (pageSize < 0)
+				{
+					data = await userInfo.ToListAsync();
+				}
+				else
+				{
+					data = await userInfo.Skip(skip).Take(pageSize).ToListAsync();
+				}
 
-        // Ordering
-        [HttpPost("ordering")]
-        public async Task<IActionResult> PostOrderingAsync()
-        {
-            try
-            {
-                // Create a config object
-                ParsingConfig config = new ParsingConfig
-                {
-                    UseParameterizedNamesInDynamicQuery = true
-                };
+				Debug.WriteLine(watch.Elapsed);
+				return Ok(new { draw, recordsFiltered = recordsTotal, recordsTotal, data });
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return BadRequest();
+				throw;
+			}
+		}
 
-                string draw = Request.Form["draw"].FirstOrDefault();
-                string start = Request.Form["start"].FirstOrDefault();
-                string length = Request.Form["length"].FirstOrDefault();
-                string sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
-                string sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
-                string searchValue = Request.Form["search[value]"].FirstOrDefault();
-                int pageSize = length != null ? Convert.ToInt32(length) : 0;
-                int skip = start != null ? Convert.ToInt32(start) : 0;
-                int recordsTotal = 0;
+		// Searching
+		[HttpPost("searching")]
+		public async Task<IActionResult> PostSearchingAsync()
+		{
+			Debug.WriteLine("> SEARCHING");
+			try
+			{
+				var watch = Stopwatch.StartNew();
+				watch.Start();
 
-                IQueryable<UserInfo> userInfo = (from dbUserInfo in _context.UserInfos select dbUserInfo);
-                recordsTotal = userInfo.Count();
+				string draw = Request.Form["draw"].FirstOrDefault();
+				string start = Request.Form["start"].FirstOrDefault();
+				string length = Request.Form["length"].FirstOrDefault();
+				string searchValue = Request.Form["search[value]"].FirstOrDefault();
+				int pageSize = length != null ? Convert.ToInt32(length) : 0;
+				int skip = start != null ? Convert.ToInt32(start) : 0;
+				int recordsTotal = 0;
 
-                if (!string.IsNullOrEmpty(searchValue))
-                {
-                    userInfo = userInfo.Where(m => m.Name.Contains(searchValue)
-                                                || m.Gender.Contains(searchValue)
-                                                || m.EyeColor.Contains(searchValue)
-                                                || m.Email.Contains(searchValue)
-                                                || m.Phone.Contains(searchValue)
-                                                || m.Company.Contains(searchValue));
-                }
+				IQueryable<UserInfo> userInfo = (from dbuserinfo in _context.UserInfos select dbuserinfo);
+				if (!string.IsNullOrEmpty(searchValue))
+				{
+					userInfo = userInfo.Where(m => m.Name.Contains(searchValue)
+												|| m.Gender.Contains(searchValue)
+												|| m.EyeColor.Contains(searchValue)
+												|| m.Email.Contains(searchValue)
+												|| m.Phone.Contains(searchValue)
+												|| m.Company.Contains(searchValue));
+				}
 
-                if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
-                {
-                    userInfo = userInfo.OrderBy(sortColumn + " " + sortColumnDirection);
-                }
+				recordsTotal = userInfo.Count();
+				Debug.WriteLine(recordsTotal);
 
-                List<UserInfo> data = pageSize < 0 ? await userInfo.ToListAsync() : await userInfo.Skip(skip).Take(pageSize).ToListAsync();
-                var jsonData = new { draw, recordsFiltered = recordsTotal, recordsTotal, data };
-                return Ok(jsonData);
+				List<UserInfo> data = new List<UserInfo>();
 
-            }
-            catch (Exception)
-            {
-                return BadRequest();
-                throw;
-            }
-        }
+				if (pageSize < 0)
+				{
+					data = await userInfo.ToListAsync();
+				}
+				else
+				{
+					data = await userInfo.Skip(skip).Take(pageSize).ToListAsync();
+				}
 
-        // All IN One
-        [HttpPost("AIO")]
-        public async Task<IActionResult> PostAIOAsync()
-        {
-            try
-            {
-                // Create a config object
-                ParsingConfig config = new ParsingConfig
-                {
-                    UseParameterizedNamesInDynamicQuery = true
-                };
+				Debug.WriteLine(watch.Elapsed);
+				return Ok(new { draw, recordsFiltered = recordsTotal, recordsTotal, data });
+			}
+			catch (Exception)
+			{
+				return BadRequest();
+				throw;
+			}
+		}
 
-                string draw = Request.Form["draw"].FirstOrDefault();
-                string start = Request.Form["start"].FirstOrDefault();
-                string length = Request.Form["length"].FirstOrDefault();
-                string sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
-                string sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
-                string searchValue = Request.Form["search[value]"].FirstOrDefault();
-                int pageSize = length != null ? Convert.ToInt32(length) : 0;
-                int skip = start != null ? Convert.ToInt32(start) : 0;
-                int recordsTotal = 0;
+		// Ordering
+		[HttpPost("ordering")]
+		public async Task<IActionResult> PostOrderingAsync()
+		{
+			Debug.WriteLine("> ORDERING");
+			try
+			{
+				var watch = Stopwatch.StartNew();
+				watch.Start();
 
-                IQueryable<UserInfo> userInfo = (from dbUserInfo in _context.UserInfos select dbUserInfo);
-                recordsTotal = userInfo.Count();
+				// Create a config object
+				ParsingConfig config = new ParsingConfig
+				{
+					UseParameterizedNamesInDynamicQuery = true
+				};
 
-                StringBuilder @condition = new StringBuilder();
-                @condition.Append("u => ");
-                PropertyInfo[] props = typeof(UserInfo).GetProperties();
-                for (int i = 0; i < props.Length; i++)
-                {
-                    if (!string.IsNullOrEmpty(searchValue) && (props[i].Name == "Name" || props[i].Name == "Age" || props[i].Name == "IsActive"))
-                    {
-                        if (props[i].PropertyType != typeof(string))
-                        {
-                            @condition.Append("u.");
-                            @condition.Append(props[i].Name);
-                            @condition.Append(".Value.ToString().Contains(\"" + searchValue + "\") || ");
-                        }
-                        else
-                        {
-                            @condition.Append("u.");
-                            @condition.Append(props[i].Name);
-                            @condition.Append(".Contains(\"" + searchValue + "\") || ");
-                        }
-                    }
-                }
-                if (!string.IsNullOrEmpty(searchValue))
-                {
-                    var whereStr = @condition.ToString().Substring(0, @condition.Length - 4);
-                    userInfo = userInfo.Where(whereStr);
-                }
+				string draw = Request.Form["draw"].FirstOrDefault();
+				string start = Request.Form["start"].FirstOrDefault();
+				string length = Request.Form["length"].FirstOrDefault();
+				string sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+				string sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+				string searchValue = Request.Form["search[value]"].FirstOrDefault();
+				int pageSize = length != null ? Convert.ToInt32(length) : 0;
+				int skip = start != null ? Convert.ToInt32(start) : 0;
+				int recordsTotal = 0;
 
+				IQueryable<UserInfo> userInfo = (from dbUserInfo in _context.UserInfos select dbUserInfo);
+				recordsTotal = userInfo.Count();
+				Debug.WriteLine(recordsTotal);
 
+				if (!string.IsNullOrEmpty(searchValue))
+				{
+					userInfo = userInfo.Where(m => m.Name.Contains(searchValue)
+												|| m.Gender.Contains(searchValue)
+												|| m.EyeColor.Contains(searchValue)
+												|| m.Email.Contains(searchValue)
+												|| m.Phone.Contains(searchValue)
+												|| m.Company.Contains(searchValue));
+				}
 
+				if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+				{
+					userInfo = userInfo.OrderBy(sortColumn + " " + sortColumnDirection);
+				}
 
-                if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
-                {
-                    userInfo = userInfo.OrderBy(sortColumn + " " + sortColumnDirection);
-                }
+				List<UserInfo> data = new List<UserInfo>();
 
-                List<UserInfo> data = pageSize < 0 ? await userInfo.ToListAsync() : await userInfo.Skip(skip).Take(pageSize).ToListAsync();
-                var jsonData = new { draw, recordsFiltered = recordsTotal, recordsTotal, data };
-                return Ok(jsonData);
+				if (pageSize < 0) {
+					data = await userInfo.ToListAsync();
+				} else
+				{
+					data = await userInfo.Skip(skip).Take(pageSize).ToListAsync();
+				}
 
-            }
-            catch (Exception ex)
-            {
-                return BadRequest();
-                throw;
-            }
-        }
-    }
+				Debug.WriteLine(watch.Elapsed);
+				var jsonData = new { draw, recordsFiltered = recordsTotal, recordsTotal, data };
+				return Ok(jsonData);
+			}
+			catch (Exception)
+			{
+				return BadRequest();
+				throw;
+			}
+		}
+
+		// All IN One
+		[HttpPost("AIO")]
+		public async Task<IActionResult> PostAIOAsync()
+		{
+			Debug.WriteLine("> AIO");
+			try
+			{
+				var watch = Stopwatch.StartNew();
+				watch.Start();
+
+				// Create a config object
+				ParsingConfig config = new ParsingConfig
+				{
+					UseParameterizedNamesInDynamicQuery = true
+				};
+
+				string draw = Request.Form["draw"].FirstOrDefault();
+				string start = Request.Form["start"].FirstOrDefault();
+				string length = Request.Form["length"].FirstOrDefault();
+				string sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+				string sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+				string searchValue = Request.Form["search[value]"].FirstOrDefault();
+				int pageSize = length != null ? Convert.ToInt32(length) : 0;
+				int skip = start != null ? Convert.ToInt32(start) : 0;
+				int recordsTotal = 0;
+
+				IQueryable<UserInfo> userInfo = (from dbUserInfo in _context.UserInfos select dbUserInfo);
+				recordsTotal = userInfo.Count();
+
+				StringBuilder @condition = new StringBuilder();
+				@condition.Append("u => ");
+				PropertyInfo[] props = typeof(UserInfo).GetProperties();
+				for (int i = 0; i < props.Length; i++)
+				{
+					if (!string.IsNullOrEmpty(searchValue) && (props[i].Name == "Name" || props[i].Name == "Age" || props[i].Name == "IsActive"))
+					{
+						if (props[i].PropertyType != typeof(string))
+						{
+							@condition.Append("u.");
+							@condition.Append(props[i].Name);
+							@condition.Append(".Value.ToString().Contains(\"" + searchValue + "\") || ");
+						}
+						else
+						{
+							@condition.Append("u.");
+							@condition.Append(props[i].Name);
+							@condition.Append(".Contains(\"" + searchValue + "\") || ");
+						}
+					}
+				}
+				if (!string.IsNullOrEmpty(searchValue))
+				{
+					var whereStr = @condition.ToString().Substring(0, @condition.Length - 4);
+					userInfo = userInfo.Where(whereStr);
+				}
+
+				if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+				{
+					userInfo = userInfo.OrderBy(sortColumn + " " + sortColumnDirection);
+				}
+
+				List<UserInfo> data = new List<UserInfo>();
+
+				if (pageSize < 0)
+				{
+					data = await userInfo.ToListAsync();
+				}
+				else
+				{
+					data = await userInfo.Skip(skip).Take(pageSize).ToListAsync();
+				}
+
+				Debug.WriteLine(watch.Elapsed);
+				var jsonData = new { draw, recordsFiltered = recordsTotal, recordsTotal, data };
+				return Ok(jsonData);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest();
+				throw;
+			}
+		}
+	}
 }
